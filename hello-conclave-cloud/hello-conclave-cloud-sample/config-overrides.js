0ae915fa647ca6931fb5f4c75546bcffc345816a
Emi Suarez <emi.suarez@r3.com>
2022-05-18 16:41:10 +0100

    prod changes

--- hello-conclave-cloud/hello-conclave-cloud-sample/config-overrides.js
+++ hello-conclave-cloud/hello-conclave-cloud-sample/config-overrides.js
@@ -3,8 +3,6 @@ module.exports = function override(config) {
   Object.assign(fallback, {
     "stream": require.resolve("stream-browserify"),
     "crypto": require.resolve("crypto-browserify"),
-    "buffer": require.resolve("buffer/"),
-     "assert": require.resolve("assert/")
   });
   config.resolve.fallback = fallback;
   return config;
--- hello-conclave-cloud/hello-conclave-cloud-sample/src/ConclaveCloud.js
+++ hello-conclave-cloud/hello-conclave-cloud-sample/src/ConclaveCloud.js
@@ -1,8 +1,8 @@
 import { Conclave } from 'conclave-cloud-sdk'
 
 const conclaveConfig = new Conclave.create({
-    tenantID: 'TDEA900FBF7F1303968BC9DE759130DCF0932E2CBC095E43EE20CCBEA9EFEFBC',
-    projectID: 'fba215b48d6ee0686f1096db851548e491b106a067c6fdc9fab50a32db53b3df',
+    tenantID: 'TE30EC967C2E77D6F01D8C41318AF9DB2717065D566237E71D1FA7EAF9187AEF',
+    projectID: 'e1e5c1401fd34ee86a88ebb56908a4939684e74bde6929bf2fe99f36152b0c22',
 });
 
 export default conclaveConfig;
